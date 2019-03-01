5a08ffe5a82f6bb9ce7d22bd01e701c19cbae012
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-03-01 23:23:48 +0700

    init note action

--- app/redux/actions/index.action.js
+++ app/redux/actions/index.action.js
@@ -6,10 +6,14 @@ import {createAction} from 'redux-actions';
 // action type
 // -----------
 
-export const TEMP_ACTION = 'TEMP_ACTION';
+export const POPULATE_NOTES_ACTION = 'POPULATE_NOTES_ACTION';
+export const INSERT_NOTE_ACTION = 'INSERT_NOTE_ACTION';
+export const UPDATE_NOTE_ACTION = 'UPDATE_NOTE_ACTION';
 
 // --------------
 // action creator
 // --------------
 
-export const tempAction = createAction(TEMP_ACTION);
+export const populateNotesAction = createAction(POPULATE_NOTES_ACTION);
+export const insertNoteAction = createAction(INSERT_NOTE_ACTION);
+export const updateNoteAction = createAction(UPDATE_NOTE_ACTION);
