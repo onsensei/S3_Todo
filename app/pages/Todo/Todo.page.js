ee2ef5ebde726184948f5f1027ec25793e216c93
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-03-02 15:18:54 +0700

    pass todo item data based on mode

--- app/pages/Todo/Todo.page.js
+++ app/pages/Todo/Todo.page.js
@@ -1,25 +1,33 @@
+import PropTypes from 'prop-types';
 import React, {Component} from 'react';
 import styles from './Todo.style';
 import Todo from '../../components/Todo/Todo.component';
 import {connect} from 'react-redux';
+import {get} from 'lodash';
 import {View} from 'react-native';
 
 class TodoPage extends Component {
   render () {
+    const {navigation} = this.props;
+    const todoItem = get(navigation, 'state.params.todoItem', null);
+    const todoMode = get(navigation, 'state.params.todoMode', null);
+    const todoItemData = todoMode === 'edit' ? todoItem : null;
     return (
-      <View>
-        <Todo style={styles.container}/>
+      <View style={styles.container}>
+        <Todo style={styles.todoContainer}
+          todoItemData={todoItemData}
+        />
       </View>
     );
   }
 }
 
 TodoPage.propTypes = {
-  //
+  navigation: PropTypes.object
 };
 
 TodoPage.defaultProps = {
-  //
+  navigation: null
 };
 
 const mapStateToProps = () => ({
